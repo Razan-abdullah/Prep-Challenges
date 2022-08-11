5265533cbd983bbc6394f548a9618375ace64300
Razan-abdullah <razanaalabdallah017@gmail.com>
2022-08-11 07:50:05 +0300

    challenge 03

---
+++ code-challenges/challenges-03.js
@@ -0,0 +1,78 @@
+"use strict";
+
+// Important:
+// To solve these challenges you have use (for in ,for of) or (Object.keys ,Object.values, Object.entries )
+// Resources:
+// for in : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...in
+// for of : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...of
+
+// -------------------------------------------------------------------------------------------------------
+//  Challenge 1:
+//  Required:
+//
+//  Write a function that takes an object and returns the formatted text based on the provided object as the example:
+// 
+// Input:
+//  const obj = {
+//      firstName: 'Ellie',
+//      lastName: 'jon',
+//      age: 67,
+//      hobby: 'Gaming and Sleeping'
+//  }
+//
+//  Output: "my name is Ellie Jon I am 67 YO, and I love Gaming and Sleeping."
+
+//  Note that:
+//  1- The text template is "my name is ** ** I am ** YO, and I love **." 
+// the output should match the above template, no extra spaces or letters.
+//  2- The first letter of the firstName and lastName should be capital letter
+
+const objLit = (obj) => {
+  // write your code here
+  let a=[];
+ let b;
+
+  for (const i in obj) {
+String(obj[i]);
+a.push(obj[i]);
+}
+
+
+  return (`my name is ${a[0].charAt(0).toUpperCase() + a[0].slice(1)} ${a[1]} I am ${a[2]} YO, and I love ${a[3]}.`);
+  
+  }
+
+// -------------------------------------------------------------------------------------------------------
+// Challenge 02:
+// Required:
+
+// You are given an object that contains name and age of some customers.
+// Write a function that returns an array of strings that follows the below structure
+//
+// the string format is "Customer Name :*** *** , Age :**"
+
+// Input:
+// const obj = {
+//     "Romio Joliat": 35,
+//     "Mario Ristrova": 39,
+//     "Sofia firnando": 50,
+// }
+//
+// Output:
+// ["Customer Name :Romio Joliat , Age :35", "Customer Name :Mario Ristrova , Age :39", ... ]
+
+const customerAndAge = (obj) => {
+  // write your code here
+let array=[];
+  for (const property in obj) {
+    array.push(`Customer Name :${property} , Age :${obj[property]}`);
+  }
+return array;
+
+};
+// -------------------------------------------------------------------------------------------------------
+
+module.exports = {
+  objLit,
+  customerAndAge
+};
